65f7acc808d6d71cdfd9ac34a7b5ec9b0fef4917
LacoAncaLaura <lala14444444@yahoo.com>
2020-06-17 13:29:44 +0300

    Initial commit

---
+++ src/main/java/com/example/awesomechat/AwesomeChatApplication.java
@@ -0,0 +1,13 @@
+package com.example.awesomechat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwesomeChatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AwesomeChatApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/awesomechat/AwesomeChatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.awesomechat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwesomeChatApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
