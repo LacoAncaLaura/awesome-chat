04821ed13e2e4f33840532aa8ff55797692bbe3a
LacoAncaLaura <lala14444444@yahoo.com>
2020-06-18 17:51:28 +0300

    .

--- src/main/java/org/fasttrackit/awesomechat/service/UserService.java
+++ src/main/java/org/fasttrackit/awesomechat/service/UserService.java
@@ -1,4 +1,24 @@
 package org.fasttrackit.awesomechat.service;
 
+import org.fasttrackit.awesomechat.domain.User;
+import org.fasttrackit.awesomechat.persistance.UserRepository;
+import org.fasttrackit.awesomechat.transfer.SaveUserRequest;
+import org.springframework.stereotype.Service;
+
+@Service
 public class UserService {
+
+    private final UserRepository userRepository ;
+
+    public UserService(UserRepository userRepository){this.userRepository = userRepository;}
+        public User createUser (SaveUserRequest request){
+            System.out.println("Creating new account: " + request);
+            User user = new User();
+            user.setLoginName(request.getLoginName());
+            user.setName(request.getName());
+            user.setAge(request.getAge());
+            user.setGender(request.getGender());
+            user.setImageURL(request.getImageURL());
+            return userRepository.save(user);
+    }
 }
--- src/main/java/org/fasttrackit/awesomechat/transfer/SaveUserRequest.java
+++ src/main/java/org/fasttrackit/awesomechat/transfer/SaveUserRequest.java
@@ -1,4 +1,66 @@
 package org.fasttrackit.awesomechat.transfer;
 
+import javax.validation.constraints.NotNull;
+
 public class SaveUserRequest {
+
+    private String loginName;
+    @NotNull
+    private String name;
+    @NotNull
+    private int age;
+    @NotNull
+    private double gender;
+    private String imageURL;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getLoginName() {
+        return loginName;
+    }
+
+    public void setLoginName(String loginName) {
+        this.loginName = loginName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public double getGender() {
+        return gender;
+    }
+
+    public void setGender(double gender) {
+        this.gender = gender;
+    }
+
+    public String getImageURL() {
+        return imageURL;
+    }
+
+    public void setImageURL(String imageURL) {
+        this.imageURL = imageURL;
+    }
+
+    @Override
+    public String toString() {
+        return "SaveUserRequest{" +
+                "loginName='" + loginName + '\'' +
+                ", name='" + name + '\'' +
+                ", age=" + age +
+                ", gender=" + gender +
+                ", imageURL='" + imageURL + '\'' +
+                '}';
+    }
 }
+
