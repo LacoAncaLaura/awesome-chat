8cdf05f992c3a04f206da29ffb9e21a4bbaa21c5
LacoAncaLaura <lala14444444@yahoo.com>
2020-08-13 11:51:43 +0300

    .

--- src/main/java/org/fasttrackit/awesomechat/configuration/WebSocketConfig.java
+++ src/main/java/org/fasttrackit/awesomechat/configuration/WebSocketConfig.java
@@ -1,46 +1,45 @@
 package org.fasttrackit.awesomechat.configuration;
 
-import org.fasttrackit.awesomechat.AwesomeChatApplication;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.WebSocketSession;
 import org.springframework.web.socket.config.annotation.*;
-import org.springframework.web.socket.handler.AbstractWebSocketHandler;
+
 
 
 @Configuration
-@EnableWebSocket
-public class WebSocketConfig implements WebSocketConfigurer {
+@EnableWebSocketMessageBroker
+public abstract class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
+//    @Override
+//    public void registerWebSocketHandlers(WebSocketHandlerRegistry webSocketHandlerRegistry) {
+//        webSocketHandlerRegistry.addHandler(new AbstractWebSocketHandler() {
+//            @Override
+//            public void afterConnectionEstablished(WebSocketSession session) throws Exception {
+//                super.afterConnectionEstablished(session);
+//            }
+//        }, "/web-socket");
+//    }
+
+    //Simple Text Oriented Messaging Protocol (STOMP)
     @Override
-    public void registerWebSocketHandlers(WebSocketHandlerRegistry webSocketHandlerRegistry) {
-        webSocketHandlerRegistry.addHandler(new AbstractWebSocketHandler() {
-            @Override
-            public void afterConnectionEstablished(WebSocketSession session) throws Exception {
-                super.afterConnectionEstablished(session);
-            }
-        }, "/web-socket");
+    public void registerStompEndpoints(StompEndpointRegistry registry){
+        registry.addEndpoint("/gs-guide-websocket").withSockJS();
+    }
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry config){
+        //        RabbitMQ
+        config.enableSimpleBroker("/topic");
+        config.enableStompBrokerRelay("/topic")
+                .setRelayHost("localhost")
+                .setRelayPort(63342)
+                .setClientLogin("guest")
+                .setClientPasscode("guest");
+
+        config.setApplicationDestinationPrefixes("/app");
+
+
+
     }
 }
-//public class WebSocketConfig  implements WebSocketMessageConfigurer {
-//Simple Text Oriented Messaging Protocol (STOMP)
-//    @Override
-//    public void registerStompEndpoints(StompEndpointRegistry stompEndpointRegistry) {
-//        stompEndpointRegistry.addEndpoint("/ws").withSockJS();
-//    }
-//
-//    @Override
-//    public void configureClientInboundChannel(org.springframework.messaging.simp.config.ChannelRegistration channelRegistration) {
-//
-//    }
-//
-//    @Override
-//    public void configureClientOutboundChannel(org.springframework.messaging.simp.config.ChannelRegistration channelRegistration) {
-//
-//    }
-//
-//    @Override
-//    public void configureMessageBroker(org.springframework.messaging.simp.config.MessageBrokerRegistry messageBrokerRegistry) {
-//        messageBrokerRegistry.setApplicationDestinationPrefixes("/app");
-//        messageBrokerRegistry.enableSimpleBroker("/topic");
-//    }
-//}
+
--- src/main/java/org/fasttrackit/awesomechat/configuration/WebSocketEventListener.java
+++ src/main/java/org/fasttrackit/awesomechat/configuration/WebSocketEventListener.java
@@ -1,37 +1,37 @@
-package org.fasttrackit.awesomechat.configuration;
-
-import io.lettuce.core.event.connection.ConnectionEvent;
-import io.lettuce.core.event.connection.DisconnectedEvent;
-import org.fasttrackit.awesomechat.domain.Chat;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-import org.springframework.context.event.EventListener;
-import org.springframework.messaging.Message;
-import org.springframework.messaging.simp.SimpMessageSendingOperations;
-import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
-import org.springframework.stereotype.Component;
-
-@Component
-public class WebSocketEventListener {
-    public static final Logger LOGGER = LoggerFactory.getLogger(WebSocketEventListener.class);
-
-    private SimpMessageSendingOperations messagingTemplate;
-
-    @EventListener
-    public void handleWebSocketConnect(ConnectionEvent event){
-        LOGGER.info("Received a new connection");
-    }
-    @EventListener
-    public void handleWebSocketDisconnect(DisconnectedEvent event){
-        StompHeaderAccessor headerAccessor = StompHeaderAccessor.wrap((Message<?>) event);
-        String loginName = headerAccessor.getContentType().getParameter("loginName");
-        if (loginName != null){
-            LOGGER.info("User disconnected" + loginName);
-            Chat chat = new Chat();
-            chat.setType(Chat.MessageType.LEAVE);
-            chat.setSender(loginName);
-            messagingTemplate.convertAndSend("/topic/public", chat);
-        }
-    }
-}
+//package org.fasttrackit.awesomechat.configuration;
+//
+//
+//import org.fasttrackit.awesomechat.domain.Chat;
+//import org.slf4j.Logger;
+//import org.slf4j.LoggerFactory;
+//
+//import org.springframework.context.event.EventListener;
+//import org.springframework.messaging.Message;
+//import org.springframework.messaging.simp.SimpMessageSendingOperations;
+//import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
+//import org.springframework.stereotype.Component;
+//import javax.sql.ConnectionEvent;
+//
+//@Component
+//public class WebSocketEventListener {
+//    public static final Logger LOGGER = LoggerFactory.getLogger(WebSocketEventListener.class);
+//
+//    private SimpMessageSendingOperations messagingTemplate;
+//
+//    @EventListener
+//    public void handleWebSocketConnect(ConnectionEvent event){
+//        LOGGER.info("Received a new connection");
+//    }
+//    @EventListener
+//    public void handleWebSocketDisconnect(DisconnectionEvent event){
+//        StompHeaderAccessor headerAccessor = StompHeaderAccessor.wrap((Message<?>) event);
+//        String loginName = headerAccessor.getContentType().getParameter("loginName");
+//        if (loginName != null){
+//            LOGGER.info("User disconnected" + loginName);
+//            Chat chat = new Chat();
+//            chat.setType(Chat.MessageType.LEAVE);
+//            chat.setSender(loginName);
+//            messagingTemplate.convertAndSend("/topic/public", chat);
+//        }
+//    }
+//}
